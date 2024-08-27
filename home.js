c3da6b0944a62644d74c3d132c0c7145214612d3
mbsabareesh <boopathisabareesh@gmail.com>
2024-08-27 11:04:02 +0530

    modify home.js

--- home.js
+++ home.js
@@ -1,3 +1,21 @@
 function addcart(){
     alert("Added To Cart")
 }
+
+/* shopimage 1,2,3 */
+
+var MainImg = document.getElementById("MainImg");
+      var smallimg = document.getElementsByClassName("small-img");
+
+      smallimg[0].onclick = function(){
+        MainImg.src = smallimg[0].src;
+      }
+      smallimg[1].onclick = function(){
+        MainImg.src = smallimg[1].src;
+      }
+      smallimg[2].onclick = function(){
+        MainImg.src = smallimg[2].src;
+      }
+      smallimg[3].onclick = function(){
+        MainImg.src = smallimg[3].src;
+      }
